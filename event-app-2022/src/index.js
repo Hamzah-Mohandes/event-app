94cc218cbb24fbe905e4f8e48e474a686faccb44
hamidesafari <hamidesafari606@gmail.com>
2022-06-01 14:38:36 +0200

    f2

--- event-app-2022/src/index.js
+++ event-app-2022/src/index.js
@@ -1,41 +1,25 @@
-<<<<<<< HEAD
 import "./index.css";
 
-<<<<<<< HEAD
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-=======
-import App from './App';
-<<<<<<< HEAD
-=======
-=======
-import './index.css';
->>>>>>> 22980654ca2d1a43d78df8ad0815371888e946b3
-import React from 'react';
-import ReactDOM  from 'react-dom/client';
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
->>>>>>> 47b81bcdc1667046511d84aff0160e13e26b26f8
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 42c742ee31d65aa1cc210c079f83120b6f5a741e
->>>>>>> 47b81bcdc1667046511d84aff0160e13e26b26f8
-=======
->>>>>>> 22980654ca2d1a43d78df8ad0815371888e946b3
